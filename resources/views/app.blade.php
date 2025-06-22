372e116273dabf868a308a43b09ebd217c45605f
minthetawaken <minthet@epik-solutions.com>
2025-06-22 20:53:35 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#1976d2" />
+    <meta name="description" content="A collaborative platform for Myanmar youth to share and develop social good ideas" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>SparkSpot - Social Good Platform</title>
+    
+    @viteReactRefresh
+    @vite(['resources/ts/index.tsx', 'resources/css/app.css'])
+</head>
+<body class="font-sans antialiased">
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+</body>
+</html> 
